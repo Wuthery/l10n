b7a1fbe813345722494df236613b04cda30efa53
jokelbaf <60827680+JokelBaf@users.noreply.github.com>
2024-05-26 01:09:29 +0300

    chore(wuthery-ts): Remove unused import

--- wuthery-ts/i18n.tsx
+++ wuthery-ts/i18n.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, createContext, useEffect } from 'react';
+import React, { useContext, useState, createContext } from 'react';
 import { setCookie } from 'cookies-next';
 
 import { fetchTranslation } from './translations';
